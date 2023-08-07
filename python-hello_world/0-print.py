06e7062b8aa973120102c43bc8bde3b41931caf0
Yusuf sani <yalkaurawing@yahoo.com>
2023-08-08 00:15:15 +0100

    #

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,3 +1,3 @@
 #guillaume@ubuntu:~/py/$ python3 0-print.py 
-hello=("\"Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle")
 #guillaume@ubuntu:~/py/$
