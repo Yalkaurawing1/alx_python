5cf4683f72685198b0dd1f4ffdffa727599e78ae
Yusuf sani <yalkaurawing@yahoo.com>
2023-08-13 21:27:55 +0100

    3-safe_print_division

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,24 +1,9 @@
 #!/usr/bin/python3
-
-# Define the function safe_print_division with two parameters a and b
 def safe_print_division(a, b):
-    # Initialize a variable result to None
-    result = None
-
-    # Use a try block to attempt the division
     try:
-        # Assign the result of a divided by b to result
-        result = a / b
-
-    # Use an except block to handle the ZeroDivisionError
+        division = a/b
     except ZeroDivisionError:
-        # Print an error message
-        print("Cannot divide by zero")
-
-    # Use a finally block to print the result
+        division = None
     finally:
-        # Print the result using string format
-        print("Inside result: {}".format(result))
-
-        # Return the result
-        return result
+        print("Inside result: {}".format(division))
+    return (division)
