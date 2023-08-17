a1b596fcfea6b6a9e36bb6ad8250c59c40b759c8
Yusuf sani <yalkaurawing@yahoo.com>
2023-08-17 13:12:27 +0100

    1-common_elements.py

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
-def common_elments(set_1, set_2):
-    return (set_1 & set_2)
+def common_elements(set_1, set_2):
+   
+    c_set = set()
+   
+    for element in set_1:
+       
+        if element in set_2:
+            c_set.add(element)
+    return c_set
