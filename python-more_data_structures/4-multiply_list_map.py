60586145f776eb01f30b48e441926a01cc79c12c
Yusuf sani <yalkaurawing@yahoo.com>
2023-08-18 23:27:38 +0100

    4-multiply list map

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
-def best_score(a_dictionary):
-    return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
