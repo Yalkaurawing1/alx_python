3d75825773de454ef97507d20c3ebdb8ee97be7c
Yusuf sani <yalkaurawing@yahoo.com>
2023-08-22 22:27:25 +0100

    1-square.py

--- python-classes/1-squre.py
+++ python-classes/1-squre.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 # 1-square.py
-"""Define a class Square."""
-
 
 class Square:
     """Represent a square."""
 
-    def __init__(self, size):
-        """Initialize a new Square.
+    def __init__(self, size=0):
+        """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
+        # Check if size is an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        # Check if size is positive
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        # Assign size to a private instance attribute
         self.__size = size
