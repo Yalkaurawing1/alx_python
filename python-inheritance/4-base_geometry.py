1dbf081c3dac5d0cf7a995058af0a522d83e3222
Yusuf sani <yalkaurawing@yahoo.com>
2023-08-26 23:09:40 +0100

    $

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -4,15 +4,15 @@
 class BaseGeometry:
     """ Define a public instance method area """
     def area(self):
-        # Raise an Exception with the message area() is not implemented
+        """ Raise an Exception with the message area() is not implemented """
         raise Exception("area() is not implemented")
 
-# Create an instance of BaseGeometry
+""" Create an instance of BaseGeometry """
 bg = BaseGeometry()
 
-# Try to call the area method
+""" Try to call the area method """
 try:
     bg.area()
 except Exception as e:
-    # Print the exception message
-    print("[{}] {}".format(e.bg.area, e))
+    """ Print the exception message """
+    print("[{}] {}".format(e.self.area, e))
