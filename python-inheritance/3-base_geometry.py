2b2b70f2b8ceaf595a1cd22d1cf73099b96bcce9
Yusuf sani <yalkaurawing@yahoo.com>
2023-08-29 12:26:07 +0100

    3-base_geometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,15 +7,8 @@ This contains the class BaseGeometry
 class BaseGeometry:
     """I do nothing but pass-by"""
     pass
-
-""" Creating an instance if geometry """
 bg = BaseGeometry()
 
-# Print the instance
 print(bg)
-
-# Print the list of attributes and methods of the instance
 print(dir(bg))
-
-# Print the list of attributes and methods of the class
 print(dir(BaseGeometry))
