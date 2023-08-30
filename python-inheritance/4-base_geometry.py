4f6efe9b57505c9672d737e965a935e5e99ee005
Yusuf sani <yalkaurawing@yahoo.com>
2023-08-30 21:16:08 +0100

    4-base geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -2,17 +2,10 @@
 """
 This module contains the class BaseGeometry
 """
+
+
 class BaseGeometry:
     """A class with method area"""
     def area(self):
         """raises an exception when called"""
         raise Exception("area() is not implemented")
-""" Create an instance of BaseGeometry """
-bg = BaseGeometry()
-
-""" Try to call the area method """
-try:
-    print(bg.area())        
-except Exception as e:
-    """ Print the exception message """
-    print("[{}] {}".format(e.self.area, e))
