8b129f498c591d1808a7b9c5e23d9e4e7b5faf3d
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-01 22:15:31 +0100

    base models

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,5 +16,5 @@ def __init__(self, id=None):
     if id is not None:
       self.id = id
     else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
