4c7fd9b4a884cd5b9c1277128e56a40e38b74670
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-02 22:44:12 +0100

    #

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,8 +13,9 @@ def __init__(self, id=None):
     """
     intiating class mrthod
     """ 
+    self.id = Base.__nb_objects
     if id is not None:
       self.id = id
     else:
         Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        
