0c3b2bf3db85e08d26e91d00cd8e1ef25fcb4447
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-02 23:02:48 +0100

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,32 +1,108 @@
 #!/usr/bin/python3
 """CLass Rectangle"""
+# models/rectangle.py
+
 from models.base import Base
 
-
 class Rectangle(Base):
-    """Definined Rectangle class"""
+    """A class that represents a rectangle shape."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """method that initializes value for rectangle object"""
-        if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
+        """A class constructor for Rectangle.
 
-        if not isinstance(y, int):
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The horizontal position of the rectangle. Defaults to 0.
+            y (int, optional): The vertical position of the rectangle. Defaults to 0.
+            id (int, optional): The id of the object. Defaults to None.
+        """
+
+        # Call the super class with id
+        super().__init__(id)
+
+        # Assign each argument to the right attribute
         self.width = width
         self.height = height
-        self.__x = x
-        self.__y = y
-        super().__init__(id
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """A getter for the width attribute.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """A setter for the width attribute.
+
+        Args:
+            value (int): The new value for the width attribute.
+        """
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """A getter for the height attribute.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """A setter for the height attribute.
+
+        Args:
+            value (int): The new value for the height attribute.
+        """
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """A getter for the x attribute.
+
+        Returns:
+            int: The horizontal position of the rectangle.
+        """
+
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """A setter for the x attribute.
+
+        Args:
+            value (int): The new value for the x attribute.
+        """
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """A getter for the y attribute.
+
+        Returns:
+            int: The vertical position of the rectangle.
+        """
+
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """A setter for the y attribute.
+
+        Args:
+            value (int): The new value for the y attribute.
+        """
+
+        self.__y = value
