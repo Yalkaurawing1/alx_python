364f916608c3784114140c5f480095fb3fac91e6
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-02 22:58:38 +0100

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,20 +2,25 @@
 
 #!/usr/bin/python3
 """This module defines a base model class."""
-
+# models/base.py
 
 class Base:
-    """
-    creating private class ettribute
-    """
+    """A base class for geometric shapes."""
+
+    # A private class attribute to count the number of objects
     __nb_objects = 0
-def __init__(self, id=None):
-    """
-    intiating class mrthod
-    """ 
-    self.id = Base.__nb_objects
-    if id is not None:
-      self.id = id
-    else:
-        Base.__nb_objects += 1
-        
+
+    def __init__(self, id=None):
+        """A class constructor for Base.
+
+        Args:
+            id (int, optional): The id of the object. Defaults to None.
+        """
+
+        # If id is not None, assign it to the public instance attribute id
+        if id is not None:
+            self.id = id
+        # Otherwise, increment __nb_objects and assign the new value to id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
