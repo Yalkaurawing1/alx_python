bb8d4cd1dd2f05b0f36fbea8df3fc15c40a5ecf1
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-06 23:20:57 +0100

    6 rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python3
+
+class BodyMetaClass(type):
+    """def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)"""
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=BodyMetaClass):
+    """
+    This class models an empty class
+    """
+    def __dir__(cls) -> None:
+        """
+        control access to some inherited attributes
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+
 """Importing from 5's BaseGeometry"""
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
