e2f6b9408cc4b9dc7c921e556b1afef0318e679b
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-06 22:31:29 +0100

    6 my github

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 
 # Import requests and sys modules
-import sys 
 import requests
+import sys
 
-url = 'https://api.github.com/user'
+# Get the username and password from the command line arguments
 username = sys.argv[1]
 password = sys.argv[2]
-parameters = (username, password)
 
-response = requests.get(url, auth=parameters)
+# Set the authentication header with basic authentication
+auth = requests.auth.HTTPBasicAuth(username, password)
+
+# Send a GET request to the GitHub API
+response = requests.get("https://api.github.com/user", auth=auth)
+
+# Try to parse the response as JSON
 try:
-    print(response.json()['id'])
-except:
-    print('None')
+    data = response.json()
+    # Check if the JSON has an id field
+    if "id" in data:
+        # Display the id
+        print(data["id"])
+    else:
+        # Display an error message
+        print("No id found")
+except ValueError:
+    # Display an error message
+    print("Not a valid JSON")
