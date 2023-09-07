8262f997dfa076953c58a025ee37a270309c50aa
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-07 09:27:09 +0100

    8 square

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -4,50 +4,38 @@ Contains the class BaseGeometry and subclass Rectangle
 """
 
 
-class BaseGeometry:
-    """A class with public instance methods area and integer_validator"""
-    def area(self):
-        """raises an exception when called"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates that value is an integer greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """A representation of a rectangle"""
-    def __init__(self, width, height):
-        """instantiation of the rectangle"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """informal string representation of the rectangle"""
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+Rectangle = __import__('7-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """A representation of a square"""
+    """
+    A class or blueprint that models a square
+    """
     def __init__(self, size):
-        """instantiation of the square"""
+        """
+        instantiate attributes to an object
+        """
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
-
-    def area(self):
-        """"returns the area of the square"""
-        return self.__size ** 2
 
     def __str__(self):
-        """informal string reepresentation of the square"""
-        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
+        """
+        Returns a string representation of the Square instance.
+        Returns:
+            str: String representation of the Square instance.
+        """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+
+    def area(self):
+        """
+        computes area of the square
+        """
+        return self.__size ** 2
