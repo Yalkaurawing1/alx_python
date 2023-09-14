45d5420082e82759ff22f679ed411d10ca78d7a0
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-14 23:15:35 +0100

    #

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,13 +15,13 @@ password = sys.argv[2]
 database = sys.argv[3]
 
 # Connect to the MySQL server on localhost at port 3306
-connector = MySQLdb.connect(user=username, passwd=password, db=hbtn_0e_0_usa)
+connector = MySQLdb.connect(user=username, passwd=password, db=database)
 
 # Create a cursor object to execute queries
 cur = connector.cursor()
 
 # Execute a query to select all states from the states table
-cur.execute("SELECT * FROM states")
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
 # Fetch all the results as a list of tuples
 rows = cur.fetchall()
