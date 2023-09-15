64dea449cd09f87e92fa3facb2031ee7062ec8f3
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-15 20:43:08 +0100

    2- my filter states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -27,7 +27,7 @@ cur.execute(query)
 rows = cur.fetchall()
 
 # Loop through each row and print it
-for data in datas:
+for name in names:
     print(data)
 
 # Close the cursor and the database connection
