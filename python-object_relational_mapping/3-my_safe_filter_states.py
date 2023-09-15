5be91d04dca1782bb4a5dc6447b7aaac84907a65
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-15 22:55:17 +0100

    #

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 """ a script that takes in arguments and displays
 all values in the states table of
 hbtn_0e_0_usa where name matches the
-argument. But this time, write one 
+argument. But this time, write one
 that is safe from MySQL injections!
 """
 if __name__ == "__main__":
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     states_data = db_cur.fetchall()
 
 for data in states_data:
-        print(data)
+    print(data)
