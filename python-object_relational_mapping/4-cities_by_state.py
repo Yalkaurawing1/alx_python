0a099861087c000048655b9a4410f495ca34d27a
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-16 21:21:44 +0100

    4. cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
 # This script lists all cities from the database hbtn_0e_4_usa
 if __name__ == "__main__":
-    # Import the MySQLdb module
     import MySQLdb
     import sys
-# Get the arguments from the command line
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
 
-# Connect to the MySQL server on localhost at port 3306
-db = MySQLdb.connect(user=username, passwd=password, db=database)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-# Create a cursor object to execute queries
-cur = db.cursor()
+    # Connect to the database
+    connector = MySQLdb.connect(user=username, passwd=password, db=database)
 
-# Execute a query to select all cities from the cities table
-cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    # a cursor to manipulate the database
+    db_cur = connector.cursor()
+    db_cur.execute("SELECT cities.id, cities.name, states.name \
+                   FROM cities, states \
+                   WHERE states.id = state_id ORDER BY cities.id")
+    states_data = db_cur.fetchall()
 
-# Fetch all the results as a list of tuples
-rows = cur.fetchall()
-
-# Loop through each row and print it
-for row in rows:
-    print(row)
-
-# Close the cursor and the database connection
-cur.close()
-db.close()
+    for data in states_data:
+        print(data)
