cd27a3a0911619d734b18832e740879d16431adb
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-19 21:14:30 +0100

    1. HBNB

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Copy of the previous task to a new
+script that starts a Flask web application
+"""
+# Import Flask
+from flask import Flask
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Define the route for /
+@app.route('/', strict_slashes=False)
+def hello():
+    # Return the message to display
+    return 'Hello HBNB!'
+
+# Define the route for /hbnb
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    # Return the message to display
+    return 'HBNB'
+
+# Run the app if this file is executed
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
