327d45cde1f1b5c93812f6051f9d65856d0798f1
Yusuf sani <yalkaurawing@yahoo.com>
2023-10-14 09:22:44 +0100

    2. Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,47 +1,53 @@
-#!/usr/bin/python3
-"""
-This module uses a REST API to get information about an employee's TODO list progress and export it in the JSON format.
-"""
-
+# Import json module
 import json
+# Import requests module
 import requests
+# Import sys module
 import sys
 
-if __name__ == "__main__":
-    # Check if the script receives an integer as a parameter
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        # Get the employee ID from the parameter
-        employee_id = sys.argv[1]
-        # Define the base URL for the API
-        base_url = "https://jsonplaceholder.typicode.com/users/"
-        # Get the employee details from the API
-        employee = requests.get(base_url + employee_id).json()
-        # Get the employee username from the details
-        employee_username = employee.get("username")
-        # Get the employee TODO list from the API
-        todos = requests.get(base_url + employee_id + "/todos").json()
-        # Create a dictionary to store the records of tasks that are owned by this employee
-        records = {}
-        # Create a list to store the tasks as dictionaries
-        tasks = []
-        # Loop through the TODO list and create a task dictionary for each task
-        for todo in todos:
-            # Get the task completed status and title from the task
-            task_completed = todo.get("completed")
-            task_title = todo.get("title")
-            # Create a task dictionary with these keys and values: task, completed, username
-            task = {"task": task_title, "completed": task_completed, "username": employee_username}
-            # Append the task dictionary to the tasks list
-            tasks.append(task)
-        # Assign the tasks list as the value of the employee ID key in the records dictionary
-        records[employee_id] = tasks
-        # Define the file name as USER_ID.json
+# Get the employee ID from the command line argument
+employee_id = sys.argv[1]
+
+# Define the base URL for the API
+base_url = "https://jsonplaceholder.typicode.com/users/"
+
+# Get the employee details from the API
+employee_response = requests.get(base_url + employee_id)
+# Check if the response is valid
+if employee_response.status_code == 200:
+    # Parse the JSON data
+    employee_data = employee_response.json()
+    # Get the employee name
+    employee_name = employee_data["name"]
+    # Get the employee username
+    employee_username = employee_data["username"]
+    # Get the employee TODO list from the API
+    todo_response = requests.get(base_url + employee_id + "/todos")
+    # Check if the response is valid
+    if todo_response.status_code == 200:
+        # Parse the JSON data
+        todo_data = todo_response.json()
+        # Initialize an empty list for storing the tasks data
+        tasks_data = []
+        # Loop through the TODO list
+        for task in todo_data:
+            # Create a dictionary with the task title, completed status and username
+            task_dict = {"task": task["title"], "completed": task["completed"], "username": employee_username}
+            # Append the dictionary to the list of tasks data
+            tasks_data.append(task_dict)
+        # Create a dictionary with the employee ID and the tasks data
+        export_data = {employee_id: tasks_data}
+        # Define the file name for exporting the data
         file_name = employee_id + ".json"
         # Open the file in write mode
-        with open(file_name, "w") as file:
-            # Dump the records dictionary to the file as a JSON string
-            json.dump(records, file)
+        with open(file_name, "w") as f:
+            # Dump the export data to the file in JSON format
+            json.dump(export_data, f)
+        # Print a success message
+        print("Data exported to {}".format(file_name))
     else:
-        # If the script does not receive an integer as a parameter, print an error message and exit
-        print("Usage: ./2-export_to_JSON.py <employee ID>")
-        sys.exit(1)
+        # Print an error message if the TODO response is invalid
+        print("Error: Could not get the TODO list for employee {}".format(employee_id))
+else:
+    # Print an error message if the employee response is invalid
+    print("Error: Could not get the details for employee {}".format(employee_id))
